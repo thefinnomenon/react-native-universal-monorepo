67b330f19e204bd460c7826fad594a41ec967ab9
Chris Finn <chris@finn.com>
2021-10-06 10:27:27 -0400

    feat(windows): sentry

--- packages/windows/index.js
+++ packages/windows/index.js
@@ -1,5 +1,19 @@
 import { AppRegistry } from 'react-native';
 import { App } from '@my-app/app';
 import { name as appName } from './app.json';
+import * as Sentry from '@sentry/react-native';
+
+Sentry.init({
+  dsn: 'https://b011133cb51645389fb69d306f1dd1f9@o371187.ingest.sentry.io/5994191',
+  // eslint-disable-next-line no-undef
+  environment: `windows-${__DEV__ ? 'dev' : 'prod'}`,
+  autoSessionTracking: true,
+  enableNative: false,
+  integrations: [
+    new Sentry.ReactNativeTracing({
+      tracingOrigins: ['localhost', 'my-site-url.com', /^\//],
+    }),
+  ],
+});
 
 AppRegistry.registerComponent(appName, () => App);
